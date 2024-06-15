24b20fb2817730f220da522e4e106e461e41c25f
Vladimir <vladimirrogalev1@gmail.com>
2024-06-15 18:50:03 +0300

    first commit

---
+++ src/components/Todos/TodoForm2.js
@@ -0,0 +1,10 @@
+function TodoFrom2() {
+  return (
+    <form>
+      <input placeholder="Enter new todo"></input>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default TodoFrom2;
